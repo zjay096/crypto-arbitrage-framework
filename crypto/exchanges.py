30c31dd39b542e51bf9ad66be7e22d3d4194b319
zjay096 <54707394+zjay096@users.noreply.github.com>
2019-09-03 18:32:41 +0200

    Update exchanges.py
    Add all "Certified Cryptocurrency Exchanges" from CCXT

--- crypto/exchanges.py
+++ crypto/exchanges.py
@@ -4,4 +4,10 @@ exchanges = {
     'binance': ccxt.binance(),
     'kucoin': ccxt.kucoin2(),
     'bittrex': ccxt.bittrex(),
+    'bitfinex': ccxt.bitfinex(),
+    'coss': ccxt.coss(),
+    'idex': ccxt.idex(),
+    'kraken': ccxt.kraken(),
+    'poloniex': ccxt.poloniex(),
+    'upbit': ccxt.upbit()
 }
